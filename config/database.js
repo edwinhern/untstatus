1b9446f4122468677bf88cc3f4f4c37c9100f74b
Edwin Hernandez <Edwinhernandez2@my.unt.edu>
2022-02-21 21:28:25 -0600

    added key

--- config/database.js
+++ config/database.js
@@ -1,3 +1,4 @@
 module.exports = {
-    database: 'mongodb+srv://ehh0025:edstriker77@cluster0.2s5jl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    database: 'mongodb+srv://ehh0025:edstriker77@cluster0.2s5jl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    secret: 'yoursecret'
 }
