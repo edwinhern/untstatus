ecb7100315a7e30bfb2ce3662cca6ad27103db73
Edwin Hernandez <Edwinhernandez2@my.unt.edu>
2022-02-21 21:44:28 -0600

    update 2

--- server.js
+++ server.js
@@ -48,9 +48,7 @@ app.get("/", (req, res) => {
   res.send("Invalid endpoint!");
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
-});
+
 
 // Start Server
 app.listen(server_port || 5000, () => {
